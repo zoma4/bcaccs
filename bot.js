174f31d2ca839f3f0f1c569a99fa8fb5662da645
zoma4 <45948124+zoma4@users.noreply.github.com>
2019-01-02 12:56:06 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready code by NoHaxJustZenile', () => {
+console.log("Discord Bot By n3k4a.?");
+  console.log('n3k4a.?')
+  console.log('n3k4a.?')
+  console.log('n3k4a.?')
+  console.log('n3k4a.?')
+  console.log('n3k4a.?')
+ 
+ 
+}); //Toxic Codes 
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (author) {
+  return author.send(' ** حرام واحد جميل مثلك م يكون عندنا .. تنورنا  (رابط الدسكورد )** ')
+ 
+}).catch(console.error)
+})
+ 
+client.login("هنا التوكين");
